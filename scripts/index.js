e3bf2819b1a1c5770bfd731921f47fe355b5a5ec
stepanamz <stepanamz@mail.ru>
2023-01-11 18:44:32 +0700

    Правки 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,25 @@
 let editProfile = document.querySelector('.profile__info-edit-button')
-let popupProfile = document.querySelector('.popup-profile')
-let closeProfile = popupProfile.querySelector('.popup-profile__close')
+let popupProfile = document.querySelector('.popup')
+let closeProfile = popupProfile.querySelector('.popup__close')
 // Находим форму в DOM
-let formElement = popupProfile.querySelector('.popup-profile__form')
+let formElement = popupProfile.querySelector('.popup__form')
 let nameInput = formElement.querySelector('input[name="name"]')
 let jobInput = formElement.querySelector('input[name="subname"]')
 
 let profileName = document.querySelector('.profile__info-name')
 let profileJob = document.querySelector('.profile__info-subname')
-
+console.log(popupProfile)
 editProfile.addEventListener('click',function(){
-  popupProfile.classList.add('popup-profile_opened')
 
+  popupProfile.classList.add('popup_opened')
+  console.log('тык')
+  console.log(popupProfile)
   nameInput.value = profileName.textContent
   jobInput.value = profileJob.textContent
 })
 
 closeProfile.addEventListener('click',function(){
-  popupProfile.classList.remove('popup-profile_opened')
+  popupProfile.classList.remove('popup_opened')
 })
 
 // Обработчик «отправки» формы, хотя пока
@@ -32,7 +34,7 @@ function handleFormSubmit (evt) {
 
   profileName.textContent = nameInput.value
   profileJob.textContent = jobInput.value
-  popupProfile.classList.remove('popup-profile_opened')
+  popupProfile.classList.remove('popup_opened')
 }
 
 // Прикрепляем обработчик к форме:
