248e71d2e4ad2cabeea3c6cae6f27021d3ce23a5
stepanamz <stepanamz@mail.ru>
2023-03-26 01:23:27 +0700

    pravki

--- scripts/index.js
+++ scripts/index.js
@@ -75,115 +75,7 @@ const validationConfig = {
   errorClass: "popup__form-error",
 };
 
-// function openPopup(popup) {
-//   // вкл - попапа
-//   // popup.classList.add("popup_opened");
-//   // document.addEventListener("keydown", closeByEscape); // Добавили слушатель и функцию закрытия по Escape
-//   classPopup.open();
-// }
 
-// function closePopup(popup) {
-//   // выкл - попапа
-//   classPopup.setEventListeners();
-//   // popup.classList.remove("popup_opened");
-//   // document.removeEventListener("keydown", closeByEscape); // Удалили функцию закрытия по Escape
-// }
-
-/*
-buttonEditProfile.addEventListener("click", function () {
-  profilePopup.open();
-  validateProfile.disableSubmitButton();
-  validateProfile.removeValidationErrors();
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-});
-
-buttonClosePopupProfile.addEventListener("click", function () {
-  profilePopup.setEventListeners();
-});
-
-// Обработчик «отправки» формы
-function submitEditProfilePopup(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  profilePopup.setEventListeners();
-  profilePopup.close();
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formEditProfile.addEventListener("submit", submitEditProfilePopup);
-
-// popup MESTO
-
-buttonMestoAdd.addEventListener("click", function () {
-  formElementMesto.reset();
-  mestoPopup.open();
-  validateMesto.disableSubmitButton();
-  validateMesto.removeValidationErrors();
-});
-
-popupMestoClose.addEventListener("click", function () {
-  mestoPopup.setEventListeners(); // закрываем попап
-});
-
-formElementMesto.addEventListener("submit", function (event) {
-  // добавление корточек из формы
-  event.preventDefault();
-
-  const titleImage = nameImageInput.value;
-  const srcImage = linkImageInput.value;
-  createCard({ name: titleImage, link: srcImage })
-
-  mestoPopup.setEventListeners(); // закрываем попап
-  mestoPopup.close();
-  formElementMesto.reset();
-});
-
-popupImage.querySelector(".popup__close").addEventListener("click", function () {
-    // закрываем попап картинки
-    imagePopup.setEventListeners();
-  });
-
-// закрытие попапа НЕ крестик
-
-// const popupsWindow = Array.from(document.querySelectorAll(".popup"));
-
-// popupsWindow.forEach((popupWindowElement) => {
-//   popupWindowElement.addEventListener("click", function (event) {
-//     // закрытие по оверлею
-//     if (event.target === event.currentTarget) {
-//       closePopup(popupWindowElement);
-//     }
-//   });
-// });
-
-// function closeByEscape(evt) {
-//   // функция закрытия по Escape
-//   if (evt.key === "Escape") {
-//     const openedPopup = document.querySelector(".popup_opened");
-//     closePopup(openedPopup);
-//   }
-// }
-
-// initialCards.forEach((item) => {
-//     createCard(item)
-// });
-
-
-function createCard(item) {
-    // Создадим экземпляр карточки
-    const card = new Card(item, ".template-card", openPopupImage);
-    // Создаём карточку и возвращаем наружу
-    const cardElement = card.generateCard();
-    // Добавляем в DOM
-    cardElements.prepend(cardElement);
-}
-
-
- */
 // функция открытия картинки
 function openPopupImage() {
   const popupWithImage = new PopupWithImage(".popup_type_image");
@@ -199,7 +91,7 @@ validateMesto.enableValidation();
 
 // Создаем экземпляр класса Section
 
-// Вынесли Renderer из создания класса, он понадобится еще и для отрисовки новых карточек
+
 const renderer = (item) => {
   // Создадим экземпляр карточки
   const card = new Card(item, ".template-card", {
@@ -219,17 +111,13 @@ const renderer = (item) => {
 const section = new Section({ items: initialCards, renderer}, ".elements");
 section.renderItems();
 
-// function submitHandler(formData) {
-//   // обработка данных формы
-//   console.log(formData);
-// }
 
 
-const profilePopup = new PopupWithForm(".popup_type_profile", {submitHandler:(item) => userInfo.setUserInfo(item)}); // вместо этого new PopupWithForm. Здесь устанавливаем новые данные пользователя.
-//Потом навесить открытие этого класса на нужную кнопку, и в этой же функции прописать внесение данных профиля в инпуты
+const profilePopup = new PopupWithForm(".popup_type_profile", {submitHandler:(item) => userInfo.setUserInfo(item)});
 
-const mestoPopup = new PopupWithForm(".popup_type_mesto", {submitHandler:(item) => renderer(item)}); // вместо этого new PopupWithForm. Здесь как раз используем renderer еще раз.
-// Потом навесить открытие этого класса на нужную кнопку
+
+const mestoPopup = new PopupWithForm(".popup_type_mesto", {submitHandler:(item) => renderer(item)});
+
 mestoPopup.setEventListeners();
 
 buttonMestoAdd.addEventListener('click', function() {
@@ -250,9 +138,9 @@ const userInfo = new UserInfo({
 userInfo.setUserInfo({
   name: nameImageInput.value,
   about: linkImageInput.value,
-}); // Присваивать значения не нужно, эта функция должна брать значения из инпутов формы
+});
 
-// Сброс ошибок форм тоже вписать в функции слушателей открытия форм
+
 
 buttonEditProfile.addEventListener('click', (event)=>{
   const user = userInfo.getUserInfo()
@@ -261,3 +149,4 @@ buttonEditProfile.addEventListener('click', (event)=>{
   profilePopup.open();
   validateProfile.removeValidationErrors();
 })
+
